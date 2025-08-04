77b9eeb6f7b7e944835916bffba7972fd4a61a99
ity4sh <itsy4sh.dev@gmail.com>
2025-08-04 23:25:09 +0530

    sidebar

--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -2,8 +2,35 @@ import Link from "next/link";
 
 export default function LeftBar() {
 	return (
-		<div className='relative px-10'>
-			<Link href='/streaming'>Streaming</Link>
+		<div className='relative px-10 py-6 text-[16px] flex flex-col'>
+			<Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				Streaming
+			</Link>
+         <Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				Design
+			</Link>
+         <Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				AI/Automation
+			</Link>
+         <Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				Streaming
+			</Link>
+         <Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				Design
+			</Link>
+         <Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				AI/Automation
+			</Link>
+         <Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				Streaming
+			</Link>
+         <Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				Design
+			</Link>
+         <Link href='/streaming' className='border-l-2 pl-2 py-2 hover:border-white/70'>
+				AI/Automation
+			</Link>
+
 		</div>
 	);
 }
